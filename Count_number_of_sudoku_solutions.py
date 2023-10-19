956787a1402cf32ff2f0295d300c21a97831900d
Trần Đăng Huy <huytd@cystack.net>
2023-10-19 08:15:28 +0700

    update

--- Count_number_of_sudoku_solutions.py
+++ Count_number_of_sudoku_solutions.py
@@ -1,63 +1,65 @@
-def is_valid(board, row, col, num):
-    # Check if the number already exists in the row
-    for i in range(9):
-        if board[row][i] == num:
-            return False
+def is_valid_in_row(grid, row, num):
+    for col in range(9):
 
-    # Check if the number already exists in the column
-    for i in range(9):
-        if board[i][col] == num:
+        if grid[row][col] == num:
             return False
-
-    # Check if the number already exists in the 3x3 sub-grid
-    start_row = (row // 3) * 3
-    start_col = (col // 3) * 3
-    for i in range(start_row, start_row + 3):
-        for j in range(start_col, start_col + 3):
-            if board[i][j] == num:
-                return False
 
     return True
 
 
-def solve_sudoku(board):
+def is_valid_in_col(grid, col, num):
     for row in range(9):
-        for col in range(9):
-            if board[row][col] == 0:
-                for num in range(1, 10):
-                    if is_valid(board, row, col, num):
-                        board[row][col] = num
-                        if solve_sudoku(board):
-                            return True
-                        board[row][col] = 0  # Undo the current placement
-                return False  # No valid number found
-    return True  # All cells filled, solution found
+
+        if grid[row][col] == num:
+            return False
+
+    return True
 
 
-def count_sudoku_solutions(board):
-    count = [0]  # Counter to keep track of the number of solutions
-
-    def solve(board, count):
-        for row in range(9):
-            for col in range(9):
-                if board[row][col] == 0:
-                    for num in range(1, 10):
-                        if is_valid(board, row, col, num):
-                            board[row][col] = num
-                            if solve_sudoku(board):
-                                count[0] += 1
-                            board[row][col] = 0  # Undo the current placement
-                    return
-
-    solve(board, count)
-    return count[0]
+def is_valid_in_subgrid(grid, row, col, num):
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if grid[start_row + i][start_col + j] == num:
+                return False
+    return True
 
 
-# Test the function
-board = []
-for _ in range(9):
-    row = list(map(int, input().split()))
-    board.append(row)
+def is_valid(grid, row, col, num):
+    return is_valid_in_row(grid, row, num) and is_valid_in_col(grid, col, num) and is_valid_in_subgrid(grid, row, col,
+                                                                                                       num)
 
-solution_count = count_sudoku_solutions(board)
-print(solution_count)
+
+def find_empty_cell(grid):
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] == 0:
+                return (i, j)
+    return None
+
+
+def count_solutions(grid):
+    cell = find_empty_cell(grid)
+    if cell == None:
+        return 1
+    row, col = cell
+    count = 0
+    for num in range(1, 10):
+        if is_valid(grid, row, col, num):
+            grid[row][col] = num
+            count += count_solutions(grid)
+            grid[row][col] = 0
+    return count
+
+
+def read_grid():
+    grid = []
+    for i in range(9):
+        row = list(map(int, input().split()))
+        grid.append(row)
+    return grid
+
+
+grid = read_grid()
+print(count_solutions(grid))
